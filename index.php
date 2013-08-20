f39dc21b2e5e86c5c0130056c1633d1bf90ca204
Tom Rhodes <hello@madeinebor.com>
2013-08-20 10:10:33 +0100

    Removed Updater

--- index.php
+++ index.php
@@ -9,35 +9,6 @@ Author: TommusRhodus
 Author URI: http://www.madeinebor.com
 */	
 
-add_action( 'init', 'github_plugin_updater_test_init' );
-function github_plugin_updater_test_init() {
-
-	include_once 'updater.php';
-
-	define( 'WP_GITHUB_FORCE_UPDATE', true );
-
-	if ( is_admin() ) { // note the use of is_admin() to double check that this is happening in the admin
-
-		$config = array(
-			'slug' => plugin_basename( __FILE__ ),
-			'proper_folder_name' => 'ebor-cpt',
-			'api_url' => 'https://api.github.com/repos/tommusrhodus/ebor-cpt',
-			'raw_url' => 'https://raw.github.com/tommusrhodus/ebor-cpt/master',
-			'github_url' => 'https://github.com/tommusrhodus/ebor-cpt',
-			'zip_url' => 'https://github.com/tommusrhodus/ebor-cpt/archive/master.zip',
-			'sslverify' => true,
-			'requires' => '3.0',
-			'tested' => '3.3',
-			'readme' => 'README.md',
-			'access_token' => '',
-		);
-
-		new WP_GitHub_Updater( $config );
-
-	}
-
-}
-
 //enqueue admin styles
 function ebor_cpt_admin_style() {
 	wp_enqueue_style( 'ebor-cpt-admin-styles', plugins_url( '/ebor-cpt-admin-styles.css' , __FILE__ ) );
